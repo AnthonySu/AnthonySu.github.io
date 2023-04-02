dcdb5ef9c8fd97bcb15cb6c5eb4416378b57bdcc
AnthonySu <anthony0312@berkeley.edu>
2023-04-02 15:19:07 -0700

    s

--- run.sh
+++ run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# jemdoc -c mysite.conf publications.jemdoc
 jemdoc -c mysite.conf index.jemdoc
 jemdoc -c mysite.conf cv.jemdoc
-# jemdoc -c mysite.conf projects.jemdoc
-
-
-# rsync -av /Users/suhaoran/Desktop/HaoranSu_site -e ssh nived.rajaraman@login.eecs.berkeley.edu:/home/eecs/nived.rajaraman/public_html
